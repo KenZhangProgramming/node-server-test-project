d58613f72dc0e0bdbebe4eea7cf2d037aeb503d0
kenzhang <kenverygood513@gmail.com>
2017-03-27 22:48:02 -0600

    First commit of the sever app to test out SSH keys

---
+++ server.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine','hbs');
+//use built in middle ware
+app.use(express.static(__dirname + '/public'));
+
+
+// Keep track of the status of the sever by using app.use
+app.use((req,res,next)=>{
+	var now = new Date().toString();
+
+	var log = `${now}: ${req.method} ${req.url}`;
+	
+	console.log(log);
+	fs.appendFile('sever.log', log + '\n',(err)=>{
+		if(err){
+			console.log('Unable to append to sever.log.')
+		}
+	});
+	next();
+});
+
+
+app.use((req,res,next)=>{
+	var now = new Date().toString();
+
+	var log = `${now}: ${req.method} ${req.url}`;
+
+	// This middle ware controls the response to be the maintainance page.	
+	res.render('maintainance.hbs',{
+		pageTitle: 'Maintainance Page',
+		welcomeMsg: 'Welcome to be fixed' 
+	});
+
+	console.log(log);
+	fs.appendFile('sever.log', log + '\n',(err)=>{
+		if(err){
+			console.log('Unable to append to sever.log.')
+		}
+	});
+});
+
+
+
+
+
+
+hbs.registerHelper('getCurrentYear',()=>{
+	//new Date().getFullYear()
+	return 'TEST';
+});
+
+
+app.get('/', (req, res) => {
+	//res.send('<h1>Hello Express</h1>');
+	res.render('home.hbs',{
+		pageTitle: 'Home Page',
+		welcomeMsg: 'Welcome to Ken Website' 
+	});
+});
+
+app.get('/about',(req,res)=>{
+	res.render('about.hbs',{
+		pageTitle: 'About Page',
+	});
+});
+
+
+// /bad - send back json with errorMsg 
+
+
+app.get('/bad',(req,res)=>{res.send(
+	{
+		name: 'Bad Request',
+		properties:[
+		'Syntax Error',
+		'There is a bug!']
+	}
+	)});
+
+
+
+app.listen(3000,() => {console.log('Sever is up on port 3000')});
+
+
+
+
+
+
+
